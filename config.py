f427472612077431caadda1ee15340f67772b403
qiujun <i@qiujun.me>
2018-05-02 10:34:19 +0800

    修改默认配置文件

--- config.py
+++ config.py
@@ -21,8 +21,8 @@ import logging
 # if you want bind ipv4 and ipv6 '::'
 # if you want bind all of ipv4 if '0.0.0.0'
 #if you want bind all of if only '4.4.4.4'
-SERVER = '0.0.0.0'
-METHOD = 'rc4-md5'
+SERVER = '::'
+METHOD = 'aes-256-cfb'
 TIMEOUT = 60
 FAST_OPEN = False
 
